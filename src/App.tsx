30b85770221381e9b18b509ef141f1202b8f37de
Patrick <106843867+patrickstaraccount@users.noreply.github.com>
2024-10-26 15:54:36 +0200

    added Viewer Interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css';
-import Viewer from './components/Viewer/Viewer';
+import "./App.css";
+import Viewer from "./components/Viewer/Viewer";
 
 function App() {
-	return (
-		<>
-			<Viewer showClock={true} />
-		</>
-	);
+  return (
+    <>
+      <Viewer
+        showClock={false}
+        countUp={true}
+        previousTimer={0}
+        remainingTimer={0}
+        startTime={new Date(new Date().setHours(14, 0, 0, 0))}
+        state="Ticking"
+        headerText={""}
+        followTimer={false}
+        endTime={new Date(new Date().setHours(18, 0, 0, 0))}
+      />
+    </>
+  );
 }
 
 export default App;
